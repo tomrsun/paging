4b3c6a598e1264f37d8c510ae9f1da8e055d3337
tomrsun <1090783017@qq.com>
2020-01-15 16:55:47 +0800

    this is a start

---
+++ js/newslist.js
@@ -0,0 +1,544 @@
+$(function () {
+
+    // 这是模拟数据
+    var wenzhanglist = [{
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        },
+        {
+            title: 'tomrsun的分页组件',
+            time: '2020-1-13',
+            text: '如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内如果你稍加留意，就会发现近期在很多小区内，悄然挂出了就会发现近期在很多小区内'
+        }
+    ]
+
+    // 数据总数可以从后台来，也可以前端计算
+    var neironglen = wenzhanglist.length
+    // 计算页码，如果有余数，页码加1，每页显示7条数据
+    var pagesnum = Math.floor(neironglen / 7)
+    // 定义初始页码，这个变量用于传到后台
+    var currentpages = '1'
+
+    // 最后一页剩余条数
+    var remain = neironglen % 7
+
+    if (neironglen % 7 != 0) {
+        pagesnum++
+    }
+
+    // 先定义上一页，下一页和第一页
+    var strup = '<button class="upye">«上一页</button>'
+    var strnext = '<button class="nextye">下一页»</button>'
+    var strone = '<div class="com selected">1</div>'
+
+    // 这两个按需求选择
+    var allpages = '<div class="allpages">共' + pagesnum + '页</div>'
+    var jumppages = '<div class="jumppages">前往<input type="text" class="numpage">页</div>'
+
+    // 插入页面
+    $('.fenye').append(allpages)
+    $('.fenye').append(strup)
+    $('.fenye').append(strone)
+
+    // 根据页数决定是否显示省略号
+    if (pagesnum <= 6) {
+
+        for (let i = 2; i <= pagesnum; i++) {
+
+            var str = '<div class="com">' + i + '</div>'
+            $('.fenye').append(str)
+        }
+
+    } else if (pagesnum > 6) {
+        for (let i = 2; i <= 4; i++) {
+            var str = '<div class="com">' + i + '</div>'
+            $('.fenye').append(str)
+
+        }
+        var elipse = '<div class="shenglue">......</div>'
+        $('.fenye').append(elipse)
+
+        var str1 = '<div class="com">' + (pagesnum - 1) + '</div>'
+        var str2 = str1 + '<div class="com">' + pagesnum + '</div>'
+        $('.fenye').append(str2)
+    }
+
+    // 插入完页码后再插入下一页等
+    $('.fenye').append(strnext)
+    $('.fenye').append(jumppages)
+
+    // 显示每页应该显示的数据条数，放入内容盒子
+    function putbox(n) {
+        for (let i = 0; i < n; i++) {
+            var str = '<li><div class="sub-list"><span class="spots"></span><a class="sub-title">'
+            var str1 = str + wenzhanglist[i].title + '</a><span class="time">'
+            var str2 = str1 + wenzhanglist[i].time + '</span></div><div class="showtext">'
+            var str3 = str2 + wenzhanglist[i].text + '</div><a href="#" class="seedetail">查看详情>></a></li>'
+
+            $('.content-box').append(str3)
+        }
+    }
+    if (currentpages == pagesnum) {
+        putbox(remain)
+    } else {
+        putbox(7)
+    }
+
+    // 给数字页码添加点击事件
+    $('.fenye .com').on('click', function () {
+        $('.fenye .com').siblings().removeClass('selected');
+        $(this).addClass('selected')
+
+        currentpages = $(this).text()
+
+    })
+
+    // 分页的几种会改变页码显示方式的方法
+    function changefenye(n) {
+        $(".shenglue").remove()
+
+        for (let i = 1; i <= 4; i++) {
+            $('.fenye .com').eq(i - 1).text(i)
+        }
+
+        var elipse1 = '<div class="shenglue">......</div>'
+
+        $('.fenye .com').eq(3).after(elipse1)
+
+        $('.fenye .com').eq(n).addClass('selected')
+    }
+
+    function changefenye2(n) {
+        $(".shenglue").remove()
+
+        $('.fenye .com').eq(0).text('1')
+
+        var elipse1 = '<div class="shenglue">......</div>'
+
+        $('.fenye .com').eq(0).after(elipse1)
+
+        for (let i = 1; i < 4; i++) {
+            $('.fenye .com').eq(i).text(pagesnum - 5 + i)
+        }
+        $('.fenye .com').eq(3).addClass('selected')
+    }
+
+    function changefenye3(n) {
+        $(".shenglue").remove()
+
+        for (let i = 0; i < 4; i++) {
+            $('.fenye .com').eq(3 - i).text(n - i)
+        }
+
+        var elipse1 = '<div class="shenglue">......</div>'
+
+        $('.fenye .com').eq(3).after(elipse1)
+
+        $('.fenye .com').eq(3).addClass('selected')
+
+    }
+    //  点击上一页触发
+    function upye(n) {
+
+        if (n == pagesnum - 2 && pagesnum > 6) {
+            // 直接点击后面页码后情况
+            changefenye2(n)
+
+        } else if (n == 4 && pagesnum > 6) {
+
+            changefenye(3)
+
+        } else if (pagesnum > 6 && n >= pagesnum - 4) {
+
+            $('.fenye .com').eq(5 - (pagesnum - n)).addClass('selected')
+
+
+        } else if (n < pagesnum - 4 && pagesnum > 6 && n > 4) {
+            changefenye3(n)
+
+        } else {
+            $('.fenye .com').eq(n - 1).addClass('selected')
+        }
+    }
+
+    // 点击下一页触发
+    function nextye(n) {
+        if (n > 4 && pagesnum > 6 && n < pagesnum - 2) {
+
+            for (let i = 0; i < 4; i++) {
+                $('.fenye .com').eq(i).text(n - 3 + i)
+            }
+
+            $('.fenye .com').eq(3).addClass('selected')
+        } else if (n == pagesnum - 2 && pagesnum > 6) {
+
+            $(".shenglue").remove()
+
+            $('.fenye .com').eq(0).text('1')
+
+            var elipse1 = '<div class="shenglue">......</div>'
+
+            $('.fenye .com').eq(0).after(elipse1)
+
+
+            for (let i = 1; i < 4; i++) {
+                $('.fenye .com').eq(i).text(pagesnum - 5 + i)
+            }
+
+            $('.fenye .com').eq(5 - (pagesnum - n)).addClass('selected')
+
+        } else if (n > pagesnum - 2 && pagesnum > 6) {
+
+            if (n == pagesnum - 1) {
+
+                $('.fenye .com').eq(4).addClass('selected')
+            } else {
+                $('.fenye .com').eq(5).addClass('selected')
+            }
+        } else {
+
+            $('.fenye .com').eq(n - 1).addClass('selected')
+        }
+    }
+
+    // 给上一页绑定点击事件
+    $('.fenye .upye').on('click', function () {
+        if (currentpages == 1) {
+            currentpages = 1
+        } else {
+
+            currentpages--
+
+            $('.fenye .com').siblings().removeClass('selected');
+            upye(currentpages)
+        }
+
+    })
+    // 给下一页绑定点击事件
+    $('.fenye .nextye').on('click', function () {
+
+        if (currentpages == pagesnum) {
+            currentpages = pagesnum
+        } else {
+            currentpages++
+
+            $('.fenye .com').siblings().removeClass('selected');
+            nextye(currentpages)
+        }
+
+    })
+
+    // 跳转页码触发
+    $('.numpage').blur(function () {
+
+        let jumpnum = $('.numpage').val()
+
+        if (jumpnum != "" && jumpnum <= pagesnum && jumpnum >= 1) {
+            currentpages = jumpnum
+            $('.fenye .com').siblings().removeClass('selected');
+            if (jumpnum <= 4) {
+                changefenye(currentpages - 1)
+            } else if (jumpnum > 4) {
+                nextye(currentpages)
+            }
+        }
+    });
+
+});
